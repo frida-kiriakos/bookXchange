634cfdf2504584ee0ef7416bf2d8f8b09ceec51c
Frida Kiriakos <kiriakos.frida@gmail.com>
2014-04-22 16:34:01 -0700

    remove tire

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,14 +1,15 @@
 class Book < ActiveRecord::Base
-	# search using tire for eastic search
-	include Tire::Model::Search
-  include Tire::Model::Callbacks
-
+	# search using eastic search
+	include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
+  
 	belongs_to :account
 	has_one :transaction
 	validates :title, presence: true
 	validates :author, presence: true
 
 	TYPES = { added: 0, exchanged: 1, sold: 2 }
+
 
 # search using sunspot
 	# searchable do
