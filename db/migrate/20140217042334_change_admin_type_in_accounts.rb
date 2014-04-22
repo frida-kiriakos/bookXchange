df3e99b7422a3c66b8e90c1adae5a1f088fa3595
Frida Kiriakos <kiriakos.frida@gmail.com>
2014-04-22 16:01:39 -0700

    modify migration script to work on pg

--- db/migrate/20140217042334_change_admin_type_in_accounts.rb
+++ db/migrate/20140217042334_change_admin_type_in_accounts.rb
@@ -1,5 +1,5 @@
 class ChangeAdminTypeInAccounts < ActiveRecord::Migration
   def change
-  	change_column :accounts, :admin, :boolean
+  	change_column :accounts, :admin, 'boolean USING CAST(admin AS boolean)'
   end
 end
